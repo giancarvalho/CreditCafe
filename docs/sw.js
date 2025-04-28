457422b1e345946fb75ede51a76654b88c00a58d
Gian <giandcarvalho@gmail.com>
2025-04-28 12:13:21 -0300

    chore: add dynamic path

--- docs/sw.js
+++ docs/sw.js
@@ -1,18 +1,23 @@
+const basePath = self.location.pathname.replace(/sw\.js$/, '');
+
+const basePath = self.location.pathname.replace(/sw\.js$/, '');
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing...');
   event.waitUntil(
     caches.open('CreditCafe').then((cache) => {
       console.log('Opened cache');
       return cache.addAll([
-        '/',
-        '/index.html',
-        './assets/index-DbTjc8lW.css',
-        './assets/index-BnpVIo-p.js',
-        './assets/coffee-144.svg'
+        `${basePath}`,
+        `${basePath}index.html`,
+        `${basePath}assets/index-DbTjc8lW.css`,
+        `${basePath}assets/index-BnpVIo-p.js`,
+        `${basePath}assets/coffee-144.svg`
       ]);
     })
   );
 });
+
 
 self.addEventListener('activate', (event) => {
   self.clients.claim();
